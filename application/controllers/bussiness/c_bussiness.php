7268d522e39323960876bd66697be1b7fabafba6
AlirioYepes <ayepes2003@yahoo.es>
2020-09-18 19:01:01 -0500

    imagenes avatar, hay qe quitar esto

---
+++ application/controllers/bussiness/c_bussiness.php
@@ -0,0 +1,28 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class c_bussines extends Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->ValidateSession();
+        
+    }
+
+    public function index() {
+        $array['menus'] = $this->M_Main->ListMenu();
+
+        $Header['menu'] = $this->load->view('Template/Menu/V_Menu', $array, true);
+        $Header['array_css'] = array(DATATABLES_CSS, SWEETALERT_CSS);
+        $this->load->view('Template/V_Header', $Header);
+        
+        $this->load->view('Template/V_Plantilla');
+
+        $Footer['sidebar_tabs'] = $this->load->view('Template/V_sidebar_tabs', null, true);
+        $Footer['array_js'] = array(DATATABLES_JS, DATATABLES_JS_B, SWEETALERT_JS);
+        $this->load->view('Template/V_Footer', $Footer);
+    }
+
+}
+
